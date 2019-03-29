461246ed662b5ab38bb37015ce70cd200d270f26
Mateusz Puczyński <mati6095@gmail.com>
2019-03-29 23:53:15 +0100

    update texts

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,9 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Color hexToColor(String code) {
-      return new Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
-    }
-
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: "Welcome to Flutter",
+        title: "Welcome pczk",
         home: new Material(
             child: new Container(
                 padding: const EdgeInsets.all(30.0),
@@ -22,31 +18,29 @@ class MyApp extends StatelessWidget {
                       child: new Column(children: [
                     Padding(padding: EdgeInsets.only(top: 140.0)),
                     new Text(
-                      'Beautiful Flutter TextBox',
+                      'Enter your note',
                       style: new TextStyle(
-                          color: hexToColor('#F2A03D'), fontSize: 25.0),
+                          color: Colors.indigo.shade400, fontSize: 25.0),
                     ),
                     new Padding(padding: EdgeInsets.only(top: 50.0)),
                     new TextFormField(
-                      autofocus: true,
                       autovalidate: true,
                       decoration: new InputDecoration(
-                        labelText: "Enter Email",
+                        labelText: "Enter text",
                         fillColor: Colors.white,
                         border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(5.0),
+                          borderRadius: new BorderRadius.circular(15.0),
                           borderSide: new BorderSide(),
                         ),
-                        //fillColor: Colors.green
                       ),
                       validator: (val) {
                         if (val.length == 0) {
-                          return "Email cannot be empty";
+                          return "Note cannot be empty";
                         } else {
                           return null;
                         }
                       },
-                      keyboardType: TextInputType.emailAddress,
+                      keyboardType: TextInputType.text,
                       style: new TextStyle(
                         fontFamily: "Poppins",
                       ),
